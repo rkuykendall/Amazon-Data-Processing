86af3c883553a87978987b87d1689ff2f0f276af
Robert Kuykendall <robert@rkuykendall.com>
2012-10-30 10:07:57 -0500

    Fixed bug in first/last review.

--- Variable_mean.pl
+++ Variable_mean.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl -w
 use DBI;
+use DateTime::Format::Strptime qw();
+
+my $parser = DateTime::Format::Strptime->new(pattern => '%F');
 
 # MySQL
 # ==================================================
@@ -32,15 +35,27 @@ while(my $product = $products->fetchrow_hashref())
   $cleanID =~ s/^\s+//;
   $cleanID =~ s/\s+$//;
   
-  $reviews = $dbh->prepare("SELECT Rating, dateReal FROM Reviews WHERE ProductID = ?");
+  $reviews = $dbh->prepare("SELECT Rating, dateReal FROM Reviews WHERE ProductID = ? ORDER BY dateReal ASC");
   $reviews->execute($cleanID);
   my $average = 0.0;
   my $numReviews = 0;
   
   $latest = "";
   @stack = qw( NULL NULL NULL NULL NULL NULL NULL NULL NULL NULL );
+  $oldest = $parser->parse_datetime('1111-11-11');
+  $newest = $parser->parse_datetime('1111-11-11');
+  
   while(my $review = $reviews->fetchrow_hashref())
   {
+    if (defined($review->{'dateReal'}) && $review->{'dateReal'} =~ m/\d{4}-\d{2}-\d{2}/) {
+      if ($oldest->year() == 1111) {
+        $oldest = $parser->parse_datetime($review->{'dateReal'});
+      }
+      $newest = $parser->parse_datetime($review->{'dateReal'});
+    } else {
+      print $review;
+    }
+    
     # print $review->{'dateReal'}."\n";
     $average = $average + $review->{'Rating'};
     $numReviews++;
@@ -48,9 +63,16 @@ while(my $product = $products->fetchrow_hashref())
     # print $average / $numReviews . "\n";
     $latest = $review->{'dateReal'};
   }
-  if ($numReviews > 0) {    
-    $statement = "UPDATE productinfo SET NumReviews = ?, Raverage0t = ?, Raverage1t = ?, Raverage2t = ?, Raverage3t = ?, Raverage4t = ?, Raverage5t = ?, Raverage6t = ?, Raverage7t = ?, Raverage8t = ?, Raverage9t = ?, Rlatest = ? WHERE id = ?;";
-    $dbh->do($statement, undef, $numReviews, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, $latest, $product->{'id'}); 
+  if ($numReviews > 0) {
+    $days = $newest->delta_days($oldest)->in_units('days');
+
+    $perday = 1;
+    if ($days > 0) {
+      $perday = ($numReviews * 1.0) / $days;
+    }
+    
+    $statement = "UPDATE productinfo SET Rrange = ?, Rperday = ?, NumReviews = ?, Raverage0t = ?, Raverage1t = ?, Raverage2t = ?, Raverage3t = ?, Raverage4t = ?, Raverage5t = ?, Raverage6t = ?, Raverage7t = ?, Raverage8t = ?, Raverage9t = ?, Rlatest = ? WHERE id = ?;";
+    $dbh->do($statement, undef, $days, $perday, $numReviews, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, $latest, $product->{'id'}); 
     ### print "ID: $product->{'ProductID'}, $average\n";
   }  
 }
