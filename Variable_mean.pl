2113ce843ab51da7a863f690b7e1f3ff7cb3cd19
Robert Kuykendall <robert@rkuykendall.com>
2012-10-30 11:26:13 -0500

    Improved code quality and added subroutines.

--- Variable_mean.pl
+++ Variable_mean.pl
@@ -2,35 +2,49 @@
 use DBI;
 use DateTime::Format::Strptime qw();
 
+# Run the following line if you get the error 'Can't load * for module DBD::mysql'
+# export DYLD_LIBRARY_PATH=/usr/local/mysql/lib/
+$dbh = DBI->connect("DBI:mysql:database=UIC-Dataset;host=127.0.0.1", "root", "", {'RaiseError' => 1});
 my $parser = DateTime::Format::Strptime->new(pattern => '%F');
 
-# MySQL
-# ==================================================
-# Run export DYLD_LIBRARY_PATH=/usr/local/mysql/lib/
-my $dbh = DBI->connect("DBI:mysql:database=UIC-Dataset;host=127.0.0.1", "root", "", {'RaiseError' => 1});
+sub get_products {
+  # && ProductID = ' 0002171856'
+  my $products = $dbh->prepare("SELECT id, ProductID FROM productinfo WHERE (ProductID IS NOT NULL && break LIKE 'BREAK-REVIEWED%')"); 
+  
+  $products->execute();
+  
+  my @products;
+  while(my $product = $products->fetchrow_hashref())
+  {
+     push @products, $product;
+  }
 
-### # execute INSERT query
-### my $rows = $dbh->do("INSERT INTO users (id, username, country) VALUES (4, 'jay', 'CZ')");
-### print "$rows row(s) affected\n";
+  return @products;
+}
 
-### # execute SELECT query
-### my $sth = $dbh->prepare("SELECT username, country FROM users");
-### $sth->execute();
+sub SQLupdate {
+  # Params: Table, ID, Values
+  my %fields = %{$_[2]};
+  
+  $statement = "UPDATE ".$_[0]." SET ";
+  
+	while (($key, $value) = each(%fields)) {
+	  $statement .= $dbh->quote($key)." = ".$dbh->quote($value).", ";
+	}
+	$statement =~ s/, $//;
+	$statement .= " WHERE id = ".$dbh->quote($_[1]).";";
+  
+  # print $statement . "\n\n"
+  $dbh->do($statement);
+}
 
-### # iterate through resultset
-### # print values
-### while(my $ref = $sth->fetchrow_hashref()) {
-###     print "User: $ref->{'username'}\n";
-###     print "Country: $ref->{'country'}\n";
-###     print "----------\n";
-### }
-# --------------------------------------------------
+@products = get_products();
 
-$products = $dbh->prepare("SELECT id, ProductID FROM productinfo WHERE (ProductID IS NOT NULL && break LIKE 'BREAK-REVIEWED%')"); # && ProductID = ' 0002171856'
-$products->execute();
-
-while(my $product = $products->fetchrow_hashref())
+foreach(@products)
 {
+  my $product = $_;
+  my %fields = ();
+  
   $cleanID = $product->{'ProductID'};
   $cleanID =~ s/^\s+//;
   $cleanID =~ s/\s+$//;
@@ -38,15 +52,15 @@ while(my $product = $products->fetchrow_hashref())
   $reviews = $dbh->prepare("SELECT Rating, dateReal FROM Reviews WHERE ProductID = ? ORDER BY dateReal ASC");
   $reviews->execute($cleanID);
   my $average = 0.0;
-  my $numReviews = 0;
+  $fields{'NumReviews'} = 0;
   
-  $latest = "";
   @stack = qw( NULL NULL NULL NULL NULL NULL NULL NULL NULL NULL );
   $oldest = $parser->parse_datetime('1111-11-11');
   $newest = $parser->parse_datetime('1111-11-11');
   
   while(my $review = $reviews->fetchrow_hashref())
   {
+
     if (defined($review->{'dateReal'}) && $review->{'dateReal'} =~ m/\d{4}-\d{2}-\d{2}/) {
       if ($oldest->year() == 1111) {
         $oldest = $parser->parse_datetime($review->{'dateReal'});
@@ -58,22 +72,24 @@ while(my $product = $products->fetchrow_hashref())
     
     # print $review->{'dateReal'}."\n";
     $average = $average + $review->{'Rating'};
-    $numReviews++;
-    push (@stack, $average / $numReviews);
-    # print $average / $numReviews . "\n";
-    $latest = $review->{'dateReal'};
+    $fields{'NumReviews'} = $fields{'NumReviews'} + 1;
+    push (@stack, $average / $fields{'NumReviews'});
+    # print $average / $fields{'NumReviews'} . "\n";
+    $fields{'Rlatest'} = $review->{'dateReal'};
   }
-  if ($numReviews > 0) {
-    $days = $newest->delta_days($oldest)->in_units('days');
+  if ($fields{'NumReviews'} > 0) {
+    $fields{'Rrange'} = $newest->delta_days($oldest)->in_units('days');
 
-    $perday = 1;
-    if ($days > 0) {
-      $perday = ($numReviews * 1.0) / $days;
+    $fields{'Rperday'} = 1;
+    if ($fields{'Rrange'} > 0) {
+      $fields{'Rperday'} = ($fields{'NumReviews'} * 1.0) / $fields{'Rrange'};
     }
     
-    $statement = "UPDATE productinfo SET Rrange = ?, Rperday = ?, NumReviews = ?, Raverage0t = ?, Raverage1t = ?, Raverage2t = ?, Raverage3t = ?, Raverage4t = ?, Raverage5t = ?, Raverage6t = ?, Raverage7t = ?, Raverage8t = ?, Raverage9t = ?, Rlatest = ? WHERE id = ?;";
-    $dbh->do($statement, undef, $days, $perday, $numReviews, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, pop @stack, $latest, $product->{'id'}); 
-    ### print "ID: $product->{'ProductID'}, $average\n";
+    foreach my $i (0..9) {
+      $fields{"Raverage".$i."t"} = pop @stack;
+    }
+    
+    $statement = SQLupdate('productinfo', $product->{'id'}, \%fields);
   }  
 }
 
@@ -82,6 +98,12 @@ while(my $product = $products->fetchrow_hashref())
 ### print "Count: ".$count."\n";
 $dbh->disconnect();
 # --------------------------------------------------
+
+
+
+
+
+
 
 
 
