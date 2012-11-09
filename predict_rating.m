2d68a6029a89a9a95f5940d6fe9b45b1aac91b1b
Robert Kuykendall <robert@rkuykendall.com>
2012-11-09 15:27:44 -0600

    Newer copy of Matlab code.

--- predict_rating.m
+++ predict_rating.m
@@ -1,20 +1,24 @@
-x = [5 4 5 5 5 4.6 4.46875 4.64705882352941 4.66666666666667 4.66666666666667 4.6 4.7 4.25 4.90909090909091 4.66666666666667 4.6 4.25 4.8 4.5 5 4.5 5 5 5 5 5 5 5 4.5 5 5 5 5 5 4 4.25 4.5 5 5 4 4.33333333333333 5 5 5 5 5 5 5 5 5 4.5 4 4.125 4.25 4.375 4.5 4.625 4.75 4.875 5 5 5 5 5 5 5 5 5 5 3 4 5 5 5 5 5 5 5 4.6 4.2 3.8 3.4 3 3.5 4 4.5 5 5 5 5 5 5 5 4 3 2 1 1 1 1];
-disp('x');
-disp(x);
+x = [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1];
+% x = ones(5,1);  % Normalized white Gaussian noise
+% noise = matrix(1, 3, [1, 2, 3]); 
+% x = filter(1,[1 1/2 1/3 1/4],noise);
+% x = x(45904:50000);
 
-a = lpc(x);
-disp('a');
-disp(a);
-
+a = lpc(x,3);
 est_x = filter([0 -a(2:end)],1,x);    % Estimated signal
-disp('est_x');
-disp(est_x);
-
 e = x - est_x;                        % Prediction error
-disp('e');
-disp(e);
-
 [acs,lags] = xcorr(e,'coeff');        % ACS of prediction error
 
+disp(x);
+disp(a);
+disp(est_x);
+disp(e);
 
-plot(1:100, x, 1:100, est_x);
+% plot(1:97,x(4001:4097),1:97,est_x(4001:4097),'--');
+% title('Original Signal vs. LPC Estimate');
+% xlabel('Sample Number'); ylabel('Amplitude'); grid;
+% legend('Original Signal','LPC Estimate')
+
+% plot(lags,acs); 
+% title('Autocorrelation of the Prediction Error');
+% xlabel('Lags'); ylabel('Normalized Value'); grid;
